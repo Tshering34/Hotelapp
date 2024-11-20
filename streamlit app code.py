6c8a3de5a8acf4389ebc1ed269e64af74dd284be
Tshering34 <tsheringpenjor45@gmail.com>
2024-11-20 16:14:51 +0600

    Update streamlit app code.py

--- streamlit app code.py
+++ streamlit app code.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-
+st.write
 # Page Configuration
 st.set_page_config(page_title="Hotel Price Analysis", layout="wide")
 
